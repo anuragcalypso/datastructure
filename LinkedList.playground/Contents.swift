0e53e4dd234807a057aad50a6dc4bb8ccf135080
Prashant Tiwari <prashant1207@gmail.com>
2019-05-15 20:54:38 +0200

    Added Function to Insert Loop in Linked List Added Function Find Loop in LinkedL List

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -73,12 +73,73 @@ func addAtPostion(head: Node?, val: Int, pos: Int) -> Node? {
     return head
 }
 
-var head = createLinkedList(length: 5)
+func insertALoopInLinkedList(head: Node?, pos: Int) -> Node? {
+    if let h = head {
+        var curr: Node? = h
+        var node: Node?
+        var i = 0
+        while(curr != nil) {
+            //save node if we reached the postion
+            if i == pos {
+                node = curr
+            }
+            
+            curr = curr?.next
+            if(curr?.next == nil) {
+                //we reached the end of the list, insert the saved node in the list
+                curr?.next = node
+                break
+            }
+            
+            i += 1
+        }
+    }
+    
+    return head
+}
+
+func loopCheckForLinkedList(head: Node?) -> Bool {
+    var result = false
+    
+    if let h = head {
+        var slow: Node? = h
+        var fast: Node? = h.next
+        var i = 0
+        while(true) {
+            if (slow == nil || fast == nil) {
+                //no loop in the list, break the loop
+                break
+            }
+            
+            if fast?.data == slow?.data {
+                result = true
+                break
+            }
+            
+            if i == 2 {
+                slow = slow?.next
+                i = 0
+            }
+            
+            fast = fast?.next
+            i += 1
+            
+            print("Fast \(fast?.data ?? -1) Slow \(slow?.data ?? -1) i \(i)")
+        }
+    }
+    
+    return result
+}
+
+//Create Linked List
+var head = createLinkedList(length: 10)
 printLinkedList(head: head)
 
-head = addAtPostion(head: head, val: 10, pos: 2)
-
-printLinkedList(head: head)
-
+//Reverse Linked List
 let revHead = reverseLinkedList(head: head)
 printLinkedList(head: revHead)
+
+//Loop Check
+head = insertALoopInLinkedList(head: head, pos: 2)
+let isLoopExist = loopCheckForLinkedList(head: head)
+print("Does Loop Exist: \(isLoopExist)")
