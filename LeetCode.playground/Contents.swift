7dabba0bbfc7d48da6c9af24c321ee89fe234007
Prashant Tiwari <prashant1207@gmail.com>
2019-06-01 11:39:17 +0200

    Added Code for Spiral Matrix

--- LeetCode.playground/Contents.swift
+++ LeetCode.playground/Contents.swift
@@ -52,6 +52,8 @@ public class ListNode {
     public init(_ val: Int) {
         self.val = val
         self.next = nil
+        let x = [Int]()
+        
     }
     
     //function to print linked list
@@ -759,5 +761,57 @@ class ReverseString {
     }
 }
 
-print(ReverseString.reverseWords("a"))
+//print(ReverseString.reverseWords("a"))
 
+
+
+class SpiralMatrix {
+    static func spiralOrder(_ matrix: [[Int]]) -> [Int] {
+        
+        var result = [Int]()
+        
+        if matrix.count == 0 || matrix[0].count == 0 {
+            return result
+        }
+        
+        var rowStart = 0
+        var rowEnd = matrix.count - 1
+        var colStart = 0
+        var colEnd = matrix[0].count - 1
+        
+        
+        while(rowStart <= rowEnd && colStart <= colEnd) {
+            for i in stride(from: colStart, through: colEnd, by: 1) {
+                result.append(matrix[rowStart][i])
+            }
+            
+            rowStart += 1
+            for i in stride(from: rowStart, through: rowEnd, by: 1) {
+                result.append(matrix[i][colEnd])
+            }
+            
+            colEnd -= 1
+            
+            if rowStart <= rowEnd {
+                for i in stride(from: colEnd, through: colStart, by: -1) {
+                    result.append(matrix[rowEnd][i])
+                }
+                
+                rowEnd -= 1
+            }
+            
+            if colStart <= colEnd {
+                for i in stride(from: rowEnd, through: rowStart, by: -1) {
+                    result.append(matrix[i][colStart])
+                }
+                
+                colStart += 1
+            }
+        }
+        
+        return result
+    }
+}
+
+let x = SpiralMatrix.spiralOrder([[[1,2,3],[4,5,6],[7,8,9]]])
+print(x)
